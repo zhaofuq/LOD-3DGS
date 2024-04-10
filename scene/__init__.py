95b152a2ed061a87f4d7029f62d1096f645664ed
samcao0416 <caojm@sari.ac.cn>
2024-04-10 15:39:03 +0800

    add depth term, no mask term

--- scene/__init__.py
+++ scene/__init__.py
@@ -43,7 +43,7 @@ class Scene:
         self.train_cameras = {}
         self.test_cameras = {}
         if os.path.exists(os.path.join(args.source_path, "sparse")):
-            scene_info = sceneLoadTypeCallbacks["Octree"](args.source_path, args.images, args.eval)
+            scene_info = sceneLoadTypeCallbacks["Octree"](args.source_path, args.images, args.depths, args.eval)
             # scene_info = sceneLoadTypeCallbacks["Colmap"](args.source_path, args.images, args.eval)
         elif os.path.exists(os.path.join(args.source_path, "transforms_train.json")):
             print("[ Scene ] Found transforms_train.json file, assuming Blender data set!")
--- train.py
+++ train.py
@@ -12,14 +12,14 @@
 import os
 import torch
 from random import randint
-from utils.loss_utils import l1_loss, ssim
+from utils.loss_utils import l1_loss, ssim # l1_loss_with_mask, ssim_with_mask
 from gaussian_renderer import render, network_gui
 import sys
 from scene import Scene, GaussianModel
 from utils.general_utils import safe_state
 import uuid
 from tqdm import tqdm
-from utils.image_utils import psnr
+from utils.image_utils import psnr, warpped_depth # psnr_with_mask
 from argparse import ArgumentParser, Namespace
 from arguments import ModelParams, PipelineParams, OptimizationParams
 try:
@@ -90,11 +90,22 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
         xyz, features, opacity, scales, rotations, cov3D_precomp, active_sh_degree, max_sh_degree, masks = scene.get_gaussuian_parameters(viewpoint_cam.world_view_transform, pipe.compute_cov3D_python)
         render_pkg = render(viewpoint_cam,  xyz, features, opacity, scales, rotations, active_sh_degree, max_sh_degree, pipe, background, cov3D_precomp = cov3D_precomp)
         image, viewspace_point_tensor, visibility_filter, radii = render_pkg["render"], render_pkg["viewspace_points"], render_pkg["visibility_filter"], render_pkg["radii"]
-
+        depth = warpped_depth(render_pkg["depth"]) # TODO: render_pkg not checked
         # Loss
         gt_image = viewpoint_cam.original_image.cuda()
         Ll1 = l1_loss(image, gt_image)
-        loss = (1.0 - opt.lambda_dssim) * Ll1 + opt.lambda_dssim * (1.0 - ssim(image, gt_image))
+        if viewpoint_cam.depth is not None:
+            gt_depth = viewpoint_cam.depth.cuda()
+
+            gt_depth_mask = viewpoint_cam.depth_mask.cuda()
+            if iteration > opt.iterations / 3:
+                gt_depth = gt_depth * gt_depth_mask + (1 - gt_depth_mask) *  0.75
+                gt_depth_mask = depth < gt_depth
+            Ll1_depth = l1_loss(depth * gt_depth_mask, gt_depth * gt_depth_mask)
+        else:
+            Ll1_depth = 0.0
+        loss = (1.0 - opt.lambda_dssim) * Ll1 + opt.lambda_dssim * (1.0 - ssim(image, gt_image)) + 0.8 * Ll1_depth
+        # loss = (1.0 - opt.lambda_dssim) * Ll1 + opt.lambda_dssim * (1.0 - ssim(image, gt_image))
         loss.backward()
 
         iter_end.record()
